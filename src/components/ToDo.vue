2986a7f9ec9d97f991886405530bd72cea3b935f
Dorcolac <nikola.starcevic@hotmail.rs>
2019-01-31 16:24:36 +0100

    init

---
+++ src/components/ToDo.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="ToDo">
+        <form action="">
+
+        </form>
+        <ul>
+            
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ToDo",
+    data () {
+        return {
+            message: 'This is a message',
+            doAlert () {
+                alert('asdads');
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
